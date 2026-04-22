3efe2a9cbab151603bfd0475fd9a6f909f130914
meihua-J <2830631273@qq.com>
2026-04-22 19:52:43 +0800

    1

--- 20260422/src/movie/dome/Movie.java
+++ 20260422/src/movie/dome/Movie.java
@@ -11,37 +11,48 @@ public class Movie {
     }
 
     public Movie(int id,String name,double price,String actor){
-
+        this.id=id;
+        this.name=name;
+        this.price=price;
+        this.actor=actor;
     }
     public int getId() {
+
         return id;
     }
 
     public void setId(int id) {
+
         this.id = id;
     }
 
     public String getName() {
+
         return name;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 
     public double getPrice() {
+
         return price;
     }
 
     public void setPrice(double price) {
+
         this.price = price;
     }
 
     public String getActor() {
+
         return actor;
     }
 
     public void setActor(String actor) {
+
         this.actor = actor;
     }
 }
--- 20260422/src/movie/dome/test.java
+++ 20260422/src/movie/dome/test.java
@@ -9,8 +9,8 @@ public class test {
         movies[3]=new Movie(4,"阿甘正传",9.5,"汤姆·汉克斯");
         movies[4]=new Movie(5,"泰坦尼克号",9.4,"莱昂纳多");
         movies[5]=new Movie(6,"盗梦空间",9.3,"莱昂纳多");
-
-        operationMovie mo =new operationMovie();
+        operationMovie mo =new operationMovie(movies);
         mo.printall();
+        mo.choiceMovie();
     }
 }
